5acc5d688edd4a9c7d607891c0e0b23eda78ad3d
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-08-09 21:05:27 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/BRANDEDKING82"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/Xeno_Kakarot"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text=" ꜱᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/lolpagalokigc"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
